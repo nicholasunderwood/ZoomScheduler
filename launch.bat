e0ead02ec9314d9d63f82ecc8fc45d243d40d2e1
Nicholas Underwood <nachos.underwood@gmail.com>
2020-12-08 11:40:50 -0800

    fixed persistant shell window

--- launch.bat
+++ launch.bat
@@ -7,13 +7,6 @@ echo %pswd%
 if defined url (
     echo running with url %url%
     start "" %url%
-    @REM if defined pswd {
-    @REM     echo running with url %url% and password %pswd%
-    @REM     python3 "C:\Users\nacho\Documents\Projects\ZoomScheduler\launch.py" %url% %pswd%
-    @REM } else (
-    @REM     echo running with url %url%
-    @REM     python3 "C:\Users\nacho\Documents\Projects\ZoomScheduler\launch.py" %url%
-    @REM )
 ) else (
     echo period not supplied
 )
@@ -22,7 +15,3 @@ if defined pswd (
     echo running with password %pswd%
     echo %pswd%| clip
 )
-pause
-
-
-@REM schtasks /create /xml ".\0.xml" /tn "\TASKSCHEDULER-FOLDER-PATH\TASK-INPORT-NAME" /ru "COMPUTER-NAME\USER-NAME"
